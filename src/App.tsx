9bdb39d8a2a25b56c0d5fa0328180a98473fab9f
vedko <vedko04@gmail.com>
2026-06-22 17:16:50 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import { Button } from '@vega-ui/react';
+
+function App() {
+
+  return (
+    <>
+      <section>
+          <Button>
+              Button
+          </Button>
+        </section>
+    </>
+  )
+}
+
+export default App
